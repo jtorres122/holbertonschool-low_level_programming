b7a7bb256a80c4700ef1066d7ded193550c6df86
Jose Torres <jose.torres122@outlook.com>
2021-06-18 17:43:46 +0000

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Return: 0
+ */
+int main(void)
+{
+	char alphbt;
+
+	for (alphbt = 'a' ; alphbt <= 'Z' ; alphbt++)
+	putchar(alphbt);
+
+	return (0);
+}
