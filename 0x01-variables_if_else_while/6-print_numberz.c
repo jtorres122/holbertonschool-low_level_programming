1709795da22dfc610a2d557a352381fcce89d69c
Jose Torres <jose.torres122@outlook.com>
2021-06-19 01:14:11 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Return: 0
+ */
+int main(void)
+{
+	int start = 48;
+	int end = 57;
+
+	for (start = 48 ; start <= end ; start++)
+	{
+		putchar(start);
+	}
+	putchar('\n');
+	return (0);
+}
