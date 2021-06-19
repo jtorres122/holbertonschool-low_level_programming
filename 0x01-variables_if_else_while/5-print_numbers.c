08a0c9ffbf17c9cb7e6ccb672aba6fd46d4ca47b
Jose Torres <jose.torres122@outlook.com>
2021-06-19 01:02:38 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Return: 0
+ */
+int main(void)
+{
+	int start = 48;
+	int end = 57;
+
+	for (start = 48 ; start <= end ; start++)
+	{
+		putchar(start);
+	}
+	return (0);
+}
