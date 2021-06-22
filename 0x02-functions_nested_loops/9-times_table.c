d62be926c4882c99652c644fb81d359cf0f06ceb
Jose Torres <jose.torres122@outlook.com>
2021-06-22 23:49:24 +0000

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * times_table - function prints the 9 times table
+ */
+void times_table(void)
+{
+	int num1, product, num2 = 0;
+
+	for (num1 = 0 ; num1 < 10 ; num1++)
+	{
+		_putchar('0');
+
+		for (num2 = 1 ; num2 < 10 ; num2++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			product = num1 * num2;
+			if (product >= 10)
+			{
+				_putchar(product / 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar(product % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
