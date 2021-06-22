0d4fc9e66fa6364a1aad0200d7a8811c86c026c6
Jose Torres <jose.torres122@outlook.com>
2021-06-22 15:22:09 +0000

    Functions & nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - function
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+
+	for (a = '97' ; z <= '122' ; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+
+	return (0);
+}
