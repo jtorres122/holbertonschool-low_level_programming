5fbbf39d67d80dc6600af391c996f4cdddf3cb39
Jose Torres <jose.torres122@outlook.com>
2021-06-23 00:01:03 +0000

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - function add two integers and returns the result
+ * @free: This was not
+ * @bee: a freebee
+ * Return: free + bee
+ */
+int add(int free, int bee)
+{
+	return (free + bee);
+}
