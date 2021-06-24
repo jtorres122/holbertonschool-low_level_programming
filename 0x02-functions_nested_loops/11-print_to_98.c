6bd61c7e9127328ab953201b7bbe64d6cd059dd1
Jose Torres <jose.torres122@outlook.com>
2021-06-24 15:28:37 +0000

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,12 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (; n >= 98 ; n--)
+		for (; n > 98 ; n--)
 		{
 			if (temp != n)
 			{
 				printf(", ");
 			}
-
 			printf("%d", n);
 		}
 	}
