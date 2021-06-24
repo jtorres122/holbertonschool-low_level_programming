746ec179246c8429e86b3f5d822c389206d68dc8
Jose Torres <jose.torres122@outlook.com>
2021-06-24 17:09:57 +0000

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - function
+ */
+void more_numbers(void)
+{
+	int loop, num1;
+
+	for (loop = 0 ; loop < 11 ; loop++)
+	{
+		for (num1 = 0 ; num1 <= 14 ; num1++)
+		{
+			if (num1 >= 10)
+			{
+				_putchar(num1 / 10 + '0');
+			}
+			_putchar(num1 % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
