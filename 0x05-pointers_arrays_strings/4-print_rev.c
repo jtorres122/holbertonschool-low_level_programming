ca9bdc61ae817c594f62d112882c5aecc9b4f457
Jose Torres <jose.torres122@outlook.com>
2021-06-28 19:20:07 +0000

    Print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_rev - function
+ * @str: parameter
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	for (count = count - 1; count >= 0 ; count--)
+	{
+		_putchar(s[count]);
+	}
+	_putchar('\n');
+}
