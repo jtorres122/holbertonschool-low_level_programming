88f20fbaf4da0852d9ef01481c9134aaecf0063e
Jose Torres <jose.torres122@outlook.com>
2021-06-28 22:53:50 +0000

    Rev String

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,7 +9,7 @@ void rev_string(char *s)
 	int length, begin;
 	char buf;
 
-	for (length = 0 ; s[length] != '\0' ; length++);
+	for (length = 0 ; s[length] != '\0' ; length++)
 
 	for (begin = 0, length-- ; begin <= length ; begin++, length--)
 	{
