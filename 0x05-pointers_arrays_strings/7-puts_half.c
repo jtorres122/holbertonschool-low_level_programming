d69e87d644c7e252c2a0a9c7cf87c3b8f74cab9e
Jose Torres <jose.torres122@outlook.com>
2021-06-29 20:14:33 +0000

    Puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,14 +9,14 @@ void puts_half(char *str)
 	int idx, n, len = 0;
 
 	for (idx = 0 ; str[idx] != '\0' ; idx++, len++)
-	;
+		;
 
 	if (len % 2 == 0)
 	{
 		n = len / 2;
-	}else
+	} else
 	{
-		n = (len - 1)/2 + 1;
+		n = (len - 1) / 2 + 1;
 	}
 
 	for (idx = n ; idx < len ; idx++)
