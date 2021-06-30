8f725cc13942f73aaaefc2355aeb005f8c905759
Jose Torres <jose.torres122@outlook.com>
2021-06-30 02:51:14 +0000

    COpy of a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy
+ * @dest: parameter
+ * @src: parameter
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int idx, len = 0;
+
+	for (idx = 0 ; src[idx] != '\0' ; idx++)
+	{
+		len++;
+	}
+
+	for (idx = 0 ; idx <= len ; idx++)
+	{
+		dest[idx] = src[idx];
+	}
+
+	return (dest);
+}
