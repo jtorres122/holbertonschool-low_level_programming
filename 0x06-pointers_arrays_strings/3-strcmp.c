fb864332ebaa24caa58ae90e79b9ea3834d034a5
Jose Torres <jose.torres122@outlook.com>
2021-07-01 19:08:47 +0000

    Comparison of two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - function
+ * @s1: parameter
+ * @s2: parameter
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int itr;
+
+	for (itr = 0 ; s1[itr] != '\0' && s2[itr] != '\0' ; itr++)
+	{
+		if (s1[itr] != s2[itr])
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
