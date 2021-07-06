f697bfae8c01e6c987e9101fb39831a293adbbd7
Jose Torres <jose.torres122@outlook.com>
2021-07-06 18:46:24 +0000

    Function copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _memset - function
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int idx;
+
+	for (idx = 0 ; idx < n ; idx++)
+	{
+		dest[idx] = src[idx];
+	}
+	return (dest);
+}
