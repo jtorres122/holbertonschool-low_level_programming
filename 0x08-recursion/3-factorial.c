e3cc11af4e1d941b37cb465ffa5efceeedfb441b
Jose Torres <jose.torres122@outlook.com>
2021-07-07 21:56:42 +0000

    Function factors variable n

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * factorial - function
+ * @n: parameter
+ * Return: 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	else
+	{
+	return (n);
+	}
+}
