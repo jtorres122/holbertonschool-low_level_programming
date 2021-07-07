f122019412be1f4a52811708ba9bae4848fe9b00
Jose Torres <jose.torres122@outlook.com>
2021-07-07 20:50:16 +0000

    Puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,10 @@ void _puts_recursion(char *s)
 	if (*s != '\0')
 	{
 		_putchar(*s);
-		s++;
+		_puts_recursion(s + 1);
 	}
 	else if (*s == '\0')
 	{
 		_putchar('\n');
-		return;
 	}
-	_puts_recursion(s);
 }
