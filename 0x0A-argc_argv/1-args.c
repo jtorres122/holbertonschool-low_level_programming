06bf190912f824b0d98e065c7ec7014bb5d06485
Jose Torres <jose.torres122@outlook.com>
2021-07-09 22:14:52 +0000

    Program prints number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ *
+ *
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
