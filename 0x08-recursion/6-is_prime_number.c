1d152423c0733916945e22ca51773fb110abfcda
Jose Torres <jose.torres122@outlook.com>
2021-07-09 03:05:26 +0000

    Function verifies if input is a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - function
+ * @n: parameter
+ * @Return: 0
+ */
+int is_prime_number(int n)
+{
+	int divisor = 2;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (prime_checker(n, divisor));
+	}
+}
+/**
+ * prime_checker - function
+ * 
+ * 
+ */
+int prime_checker(int n, int divisor)
+{
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+	if (n % divisor > 1)
+	{
+		return (1);
+	}
+	return (prime_checker(n, divisor + 1));
+}
