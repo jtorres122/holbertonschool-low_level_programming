409d8fdb0e31690a9233c058908fb2e0df0f7a40
Jose Torres <jose.torres122@outlook.com>
2021-07-13 20:16:46 +0000

    Function returns a pointer to a newly allocate space

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,14 +13,14 @@ char *_strdup(char *str)
 	for (len1 = 0 ; str[len1] != '\0' ; len1++)
 	;
 
-	newstr = malloc(sizeof(char) * len1);
+	newstr = malloc(sizeof(char) * len1 + 1);
 
 	if (newstr == NULL)
 	{
 		return (NULL);
 	}
 
-	for (len2 = 0 ; len2 <= len1 ; len2++)
+	for (len2 = 0 ; len2 < len1 ; len2++)
 	{
 		newstr[len2] = str[len2];
 	}
