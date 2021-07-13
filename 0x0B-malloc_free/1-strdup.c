8360725ce01aed1da451db01f9dbdb1f5be0bda7
Jose Torres <jose.torres122@outlook.com>
2021-07-13 20:06:49 +0000

    Function returns a pointer to a newly allocate space

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strdup -function
+ * @str: parameter
+ *
+ */
+char *_strdup(char *str)
+{
+	unsigned int len1, len2;
+	char *newstr;
+
+	for (len1 = 0 ; str[len1] != '\0' ; len1++)
+	;
+
+	newstr = malloc(sizeof(char) * len1);
+
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (len2 = 0 ; len2 < len1 ; len2++)
+	{
+		newstr[len2] = str[len2];
+	}
+
+	return (newstr);
+}
