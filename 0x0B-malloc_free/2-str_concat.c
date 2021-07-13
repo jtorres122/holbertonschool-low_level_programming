789f3a1ecebdb36216051df176afcfb2e9975770
Jose Torres <jose.torres122@outlook.com>
2021-07-13 21:20:23 +0000

    Function concatenates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * str_concat - function
+ * @s1: parameter
+ * @s2: parameter
+ * Return: char
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int len1, len2, len3, len4;
+	int sum;
+	char *s3;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	else if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (len1 = 0 ; s1[len1] != '\0' ; len1++)
+	;
+
+	for (len2 = 0 ; s2[len2] != '\0' ; len2++)
+	;
+
+	sum = len1 + len2;
+
+	s3 = malloc(sizeof(char) * sum + 1);
+
+	for (len3 = 0 ; s1[len3] != '\0' ; len3++)
+	{
+		s3[len3] = s1[len3];
+	}
+
+	for (len4 = 0 ; s2[len4] != '\0' ; len3++, len4++)
+	{
+		s3[len3] = s2[len4];
+	}
+
+	return (s3);
+}
