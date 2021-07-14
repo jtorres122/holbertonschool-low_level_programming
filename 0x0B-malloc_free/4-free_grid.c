ac91af667ee01c551fe0205ccc88adcd73dbb628
Jose Torres <jose.torres122@outlook.com>
2021-07-14 02:48:03 +0000

    Function frees 2D grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * free_grid - function
+ * @grid: parameter
+ * @height: parameter
+ */
+void free_grid(int **grid, int height)
+{
+	for (; height >= 0 ; height--)
+	{
+		free(grid[height]);
+	}
+	free(grid);
+}
