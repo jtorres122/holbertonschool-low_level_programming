d754d008fb16b61e6cb7061034af610961325897
Jose Torres <jose.torres122@outlook.com>
2021-07-19 20:36:10 +0000

    Function initializes struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - function
+ * @d: parameter
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
