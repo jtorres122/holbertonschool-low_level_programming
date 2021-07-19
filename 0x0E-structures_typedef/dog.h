09fd5d60a75781cf49f8bbd8f0d738399c454713
Jose Torres <jose.torres122@outlook.com>
2021-07-19 20:07:43 +0000

    Struct dog defined

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
 	char *name;
