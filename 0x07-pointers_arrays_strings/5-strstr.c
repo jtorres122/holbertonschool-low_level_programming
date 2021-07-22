60c6794595f21dc6b92f65d08e0f32c96d47fcd3
Jose Torres <jose.torres122@outlook.com>
2021-07-22 19:56:31 +0000

    Fix header file extension and update old files

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,18 +8,5 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int idx, idx2;
-
-	for (idx = 0 ; haystack[idx] != '\0' ; idx++)
-	{
-		for (idx2 = 0 ; needle[idx2] != '\0' ; idx2++)
-		{
-			if (haystack[idx] == needle[idx2])
-			{
-				haystack += idx;
-				return (haystack);
-			}
-		}
-	}
-	return ('\0');
+	
 }
