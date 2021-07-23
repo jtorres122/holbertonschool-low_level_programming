83a80e1a457815fe443113e7efaf33262b5cb820
Jose Torres <jose.torres122@outlook.com>
2021-07-23 03:08:32 +0000

    Function will prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - function
+ * @format: parameter
+ */
+void print_all(const char * const format, ...)
+{
+	int parse = 0;
+	char *str, *comma = "";
+	va_list list;
+
+	va_start(list, format);
+
+	while (format[parse] != '\0')
+	{
+		switch (format[parse])
+		{
+			case 'c':
+				printf("%s%c", comma, va_arg(list, int));
+				break;
+
+			case 'i':
+				printf("%s%i", comma, va_arg(list, int));
+				break;
+
+			case 'f':
+				printf("%s%f", comma, va_arg(list, double));
+				break;
+
+			case 's':
+				str = va_arg(list, char *);
+				if (str != NULL)
+					printf("%s%s", comma, str);
+				else
+					printf("%s(nil)", comma);
+				break;
+		}
+		comma = ", ";
+		parse++;
+	}
+	printf("\n");
+
+	va_end(list);
+}
