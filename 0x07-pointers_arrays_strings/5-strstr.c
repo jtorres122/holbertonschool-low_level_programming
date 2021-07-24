a42446ed486a2814dad60d6ab8c67c6e7f8e3cdc
Jose Torres <jose.torres122@outlook.com>
2021-07-24 01:30:50 +0000

    Locates a substring inside the string haystack

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,5 +8,24 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	
+	int idx1, mover,idx2 = 0;
+
+	if (*needle == '\0')
+		return (NULL);
+
+	for (idx1 = 0 ; haystack[idx1] != '\0' ; idx1++)
+	{
+		mover = idx1;
+		while (haystack[idx1] == needle[idx2])
+		{
+			if (needle[idx2 + 1] == '\0')
+			{
+				return (haystack += mover);
+			}
+			idx1++;
+			idx2++;
+		}
+		idx1 = mover;
+	}
+	return (NULL);
 }
