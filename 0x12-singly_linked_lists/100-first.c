23f6f38d525034f8ee2efa3ddd09b158b7af1b56
Jose Torres <jose.torres122@outlook.com>
2021-07-31 21:48:08 +0000

    Function prints out before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * first - function
+ */
+__attribute__ ((constructor)) void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
