08b19041861ca4a131375ec955b214f0eb629d61
Jose Torres <jose.torres122@outlook.com>
2021-08-04 03:11:12 +0000

    Function inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,6 +28,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	temp = *head;
+
+	if (temp->next == NULL)
+		return (NULL);
+
 	while (temp != NULL)
 	{
 		if (counter == idx)
