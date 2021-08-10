8893e3ee83aff4d9a039d702e3a654e8a13ca041
Jose Torres <jose.torres122@outlook.com>
2021-08-10 20:41:22 +0000

    First commit; it includes the read me file, header file and task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * read_textfile- function
+ * @filename: parameter
+ * @letters: parameter
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, len, counter;
+	char *buf;
+
+	buf = malloc(letters);
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	len = read(fd, buf, letters);
+
+	if (fd == -1 || len == -1)
+		return (0);
+
+	counter = write(STDOUT_FILENO, buf, len);
+
+	if (counter == -1)
+		return (0);
+
+	return (counter);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,7 @@
+<h1 align="center">More singly linked lists</h1>
+
+## Description
+
+In this holberton school project, we learn:
+* How to use linked lists more indepth
+* Start to use linked lists without much research
