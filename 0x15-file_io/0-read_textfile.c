7d9b594354820a314323d03e819e608623b6ab76
Jose Torres <jose.torres122@outlook.com>
2021-08-10 20:45:39 +0000

    added read permissions to open system call

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY, 0600);
 	len = read(fd, buf, letters);
 
 	if (fd == -1 || len == -1)
