89a871d5146eb40747fa5a495a1b67c3d9e2cd3c
Jose Torres <jose.torres122@outlook.com>
2021-09-17 21:18:31 +0000

    Free a list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function
+ * @head: parameter
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
