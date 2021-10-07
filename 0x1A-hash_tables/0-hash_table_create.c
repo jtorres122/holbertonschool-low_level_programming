4606d33cf12b131e8fac526feefb280cee333a26
Jose Torres <jose.torres122@outlook.com>
2021-10-07 19:28:52 +0000

    Be the first commit with README, header file and first attempt at Task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function creates a hash table
+ * @size: parameter size is the size of the array
+ * Return: Pointer to the newly created hash table or NULL if something went wrong
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash = NULL;
+	long unsigned int idx;
+
+	if (size == 0)
+		return (NULL);
+
+	new_hash = malloc(sizeof(hash_table_t));
+	if (new_hash == NULL)
+		return (NULL);
+
+	new_hash->size = size;
+
+	new_hash->array = malloc(sizeof(new_hash->array) * size);
+	if (new_hash->array == NULL)
+		return (NULL);
+
+	for (idx = 0; idx < size ; idx++)
+	{
+		new_hash->array[idx] = NULL;
+	}
+
+	return (new_hash);
+}
