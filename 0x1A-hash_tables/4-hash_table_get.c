5b80b4123602c02c19513ad6af6f58064027f7a5
Jose Torres <jose.torres122@outlook.com>
2021-10-11 02:28:44 +0000

    Add a conditional statement for ht

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = key_index((unsigned char *)key, ht->size);
 	hash_node_t *node = ht->array[index];
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
 	if (node != NULL && index <= ht->size)
 		if (strcmp(node->key, key) == 0)
 			return (node->value);
