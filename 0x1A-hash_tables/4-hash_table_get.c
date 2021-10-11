1199db7b8fc5e0e3d9488c11022a6cd1c04fdeed
Jose Torres <jose.torres122@outlook.com>
2021-10-11 02:12:04 +0000

    Push first attempt of a function that retrieves a value associated with a key in a hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function retrieves a value associated with a key
+ * @ht: parameter ht is the hash table function will look into
+ * @key: parameter key is the key function is looking for
+ * Return: value associated with element or NULL if key is not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((unsigned char *)key, ht->size);
+	hash_node_t *node = ht->array[index];
+
+	if (node != NULL)
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+
+	return (NULL);
+}
