788b312962da996ab082afd1ac2753b4aa8af560
Jose Torres <jose.torres122@outlook.com>
2021-10-11 01:31:49 +0000

    Attempt to handle malloc return

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,9 +11,9 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node = NULL, *aux_node = NULL;
-	unsigned long int index = key_index((const unsigned char *) key, ht->size);
+	unsigned long int index = key_index((unsigned char *) key, ht->size);
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (ht == NULL || key == NULL || value == NULL || strlen(key == 0)
 		return (0);
 
 	aux_node = ht->array[index];
