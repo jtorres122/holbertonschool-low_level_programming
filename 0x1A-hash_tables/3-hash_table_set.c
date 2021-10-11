56186093ff2589ef6f04b9347eaa4349173f968c
Jose Torres <jose.torres122@outlook.com>
2021-10-11 01:52:48 +0000

    Verify if conditional statement is of importance

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL;
 	hash_node_t *aux_node = NULL;
 
-	if (key == NULL || value == NULL || ht == NULL)
+	if (key == NULL || value == NULL)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
