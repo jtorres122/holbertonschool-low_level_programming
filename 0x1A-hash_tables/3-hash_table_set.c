37b76cf41b3ef4c5338510e97260c8bb7afd6bfd
Jose Torres <jose.torres122@outlook.com>
2021-10-11 01:29:26 +0000

    Attempt to handle malloc return

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -29,7 +29,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	if (!new_node)
 	{
 		free(new_node);
 		return (0);
